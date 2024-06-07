3e1c0658533801baf15f62fc897cda2ecf5ece58
FeI-VeN <ros93@inbox.ru>
2024-06-07 12:21:56 +0300

    comment

--- config/db.php
+++ config/db.php
@@ -1,13 +1,5 @@
 <?php
 
+require CONFIG . '/no_see.php';
 
-return [
-    'host'     => 'localhost',
-    'dbname'   => '*****',
-    'username' => '*****',
-    'password' => '*****',
-    'charset'  => 'utf8', //utf8mb4
-    'options'  => [
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    ],
-];
+return $db_date;
