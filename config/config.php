66a756986a422c9788fc0acf9de00844832f3c3a
FeI-VeN <ros93@inbox.ru>
2024-06-07 17:13:21 +0300

    new

--- config/config.php
+++ config/config.php
@@ -1,10 +1,11 @@
 <?php
 
 define("ROOT", dirname(__DIR__));
-define("PUBLIC", ROOT . '/public');
+define("WWW", ROOT . '/public');
 define("CONFIG", ROOT . '/config');
 define("CORE", ROOT . '/core');
 define("APP", ROOT . '/app');
 define("CONTROLLERS", ROOT . '/app/controllers');
 define("VIEWS", ROOT . '/app/views');
+define("MODELS", ROOT . '/app/models');
 define("PATH", ROOT . 'https://test-fin.s-sobakoy-na-more.ru');
--- core/functions.php
+++ core/functions.php
@@ -20,3 +20,19 @@ function abort($code = 404){
     require VIEWS . "/errors/{$code}.php";
     die;
 }
+
+function getIp() {
+    $keys = [
+        'HTTP_CLIENT_IP',
+        'HTTP_X_FORWARDED_FOR',
+        'REMOTE_ADDR'
+    ];
+    foreach ($keys as $key) {
+        if (!empty($_SERVER[$key])) {
+            $ip = trim(end(explode(',', $_SERVER[$key])));
+            if (filter_var($ip, FILTER_VALIDATE_IP)) {
+                return $ip;
+            }
+        }
+    }
+}
